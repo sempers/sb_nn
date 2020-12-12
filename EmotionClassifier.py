78866370f53b7fa145c0bbaa62950e07aefe72bb
sempers <groddenator@gmail.com>
2020-12-12 21:08:05 +0300

    fix

--- EmotionClassifier.py
+++ EmotionClassifier.py
@@ -10,7 +10,7 @@ class EmotionClassifier:
 		print("NN model loaded")    
 
 	#препроцессинг
-	def preprocess_image(x):
+	def preprocess_image(self, x):
 		x_temp = x.astype(np.float16)
 		#x_temp = x_temp[..., ::-1] #BGR уже приходит из OpenCV
 		x_temp[..., 0] -= 91.4953
@@ -18,7 +18,7 @@ class EmotionClassifier:
 		x_temp[..., 2] -= 131.0912
 		return np.expand_dims(x_temp, 0)
 
-	def predict(img):
+	def predict(self, img):
 		now = datetime.datetime.now()
 		img = self.preprocess_image(img)
 		y = self.model.predict(img)[0]
